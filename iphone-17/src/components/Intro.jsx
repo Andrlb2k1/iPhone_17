2cd5275c5f2fea660cade75c4c8c4281e0a1d389
unknown <andre.lb865854@gmail.com>
2025-10-26 09:11:58 -0300

    intro cards

--- iphone-17/src/components/Intro.jsx
+++ iphone-17/src/components/Intro.jsx
@@ -17,11 +17,11 @@ function Intro() {
                 <button className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-4 rounded-full text-lg font-medium transition-all duration-300 hover:scale-105 shadow-lg shadow-blue-500/50 cursor-pointer">Comprar agora</button>
                 <button className="border-2 border-white rounded-full hover:bg-white hover:text-black text-white px-8 py-4 text-lg font-medium transition-all duration-300 hover:scale-105 cursor-pointer">Saiba mais</button>
             </div>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 ">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-6 mx-auto max-w-5xl">
                 {specs.map((spec, index) => (
                     <div key={index} className="bg-gray-900 rounded-2xl p-6 hover:bg-gray-800 transition-all duration-300 hover:scale-105 cursor-pointer">
-                        <p>{spec.value}</p>
-                        <p>{spec.label}</p>
+                        <p className={`text-3xl font-bold ${spec.color}`}>{spec.value}</p>
+                        <p className="text-sm text-gray-300 mt-2">{spec.label}</p>
                     </div>
                 ))}
             </div>
